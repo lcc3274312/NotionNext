f6b3224961ee967a1f9cef40584a11ec13df7ff3
宇洋 <bil3278812@gmail.com>
2025-03-18 11:58:53 +0800

    f892a6e7ffaf4077afaf1d7344c01052

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@
 
 const BLOG = {
   // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
-  NOTION_PAGE_ID: process.env.NOTION_PAGE_ID || '',
+  NOTION_PAGE_ID: process.env.NOTION_PAGE_ID || 'f892a6e7ffaf4077afaf1d7344c01052',
 THEME: process.env.NEXT_PUBLIC_THEME || 'next', // 当前主题，在themes文件夹下可找到所有支持的主题；主题名称就是文件夹名，例如 example,fukasawa,gitbook,heo,hexo,landing,matery,medium,next,nobelium,plog,simple
   LANG: process.env.NEXT_PUBLIC_LANG || 'zh-TW', // e.g 'zh-CN','en-US'  see /lib/lang.js for more.
   SINCE: process.env.NEXT_PUBLIC_SINCE || 2021, // e.g if leave this empty, current year will be used.
